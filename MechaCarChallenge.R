e2d2707f7e2d0176dd817ca41be0e40e6a3bf0fb
eneubauer2022 <eneubauer@csiusa.ws>
2022-09-27 09:37:47 -0400

    deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -37,3 +37,24 @@ lot_summary <- mecha_suscoil %>% group_by(Manufacturing_Lot)%>% summarize(Mean_P
                                                                           Median_PSI=median(PSI),
                                                                           Var_PSI=var(PSI),
                                                                           Std_Dev_PSI=sd(PSI), .groups='keep')
+
+# Deliverable 3
+
+# 1. In your MechaCarChallenge.RScript, write an RScript using the t.test() function to determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch.
+t.test(mecha_suscoil$PSI,mu=1500)
+
+# 2. Next, write three more RScripts in your MechaCarChallenge.RScript using the t.test() function and its subset() argument to determine if the PSI for each manufacturing lot is statistically different from the population mean of 1,500 pounds per square inch.
+
+lot1 <- subset(mecha_suscoil, Manufacturing_Lot=="Lot1")
+
+t.test(lot1$PSI, mu=1500)
+
+
+lot2 <- subset(mecha_suscoil, Manufacturing_Lot=="Lot2")
+
+t.test(lot2$PSI, mu=1500)
+
+
+lot3 <- subset(mecha_suscoil, Manufacturing_Lot=="Lot3")
+
+t.test(lot3$PSI, mu=1500)
